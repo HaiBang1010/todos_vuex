3290db8e7b0ba29d8df4a7617a6d4732dec7fa1f
HaiBang1010 <haibang1010999@gmail.com>
2023-08-11 18:19:37 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app">
+		<Navbar />
+		<Todos />
+	</div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+import Navbar from './components/Navbar'
+
+export default {
+	name: 'App',
+	components: { Todos, Navbar }
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	background: rgb(219, 219, 219);
+	margin: 20px auto;
+	width: 100%;
+	max-width: 800px;
+}
+body{
+	width: auto;
+	height: 100%;
+	background: url('./assets/background.jpg') no-repeat fixed 	 ;
+	background-size: 100%;
+}
+</style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,57 @@
+<template>
+	<form @submit="onSubmit">
+		<input type="text" v-model="title" />
+		<button class="submit" value="Add">ADD</button>
+	</form>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import { v4 as uuidv4 } from 'uuid'
+
+export default {
+	name: 'TodoForm',
+	data() {
+		return {
+			title: ''
+		}
+	},
+	methods: {
+		...mapActions(['addTodo']), // addTodo() === this.$store.dispatch('addTodo')
+		onSubmit(event) {
+			event.preventDefault()
+			// console.log(this.title)
+			this.addTodo({
+				id: uuidv4(),
+				title: this.title,
+				completed: false
+			})
+			this.title = ''
+		}
+	}
+}
+</script>
+
+<style>
+form {
+	padding: 0px;
+	text-align: center;
+}
+
+input[type='text'] {
+	width: 50%;
+	box-sizing: border-box;
+	padding: 10px;
+	margin: 6px 10px;
+	border: 0;
+	border-radius: 6px;
+}
+
+.submit {
+	margin: 10px auto;
+	cursor: pointer;
+	padding: 10px;
+	border: 0;	
+	border-radius: 6px;
+}
+</style>
